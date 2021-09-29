d8b65594945f48494522d44b5ea1a3e9cd087940
ukins <yanghuixiong13@gmail.com>
2021-09-30 00:04:09 +0800

    123

---
+++ WeaponHuiXuanBiao.cs
@@ -0,0 +1,17 @@
+﻿using Hunter.Character.Weapon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter.Character
+{
+    /// <summary>
+    /// 回旋镖
+    /// </summary>
+    public class WeaponHuiXuanBiao : CommonWeapon
+    {
+        public WeaponHuiXuanBiao(UIAvatar avatar) : base(avatar) { }
+    }
+}
